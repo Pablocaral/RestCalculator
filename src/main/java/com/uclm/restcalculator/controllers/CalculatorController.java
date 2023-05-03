895a8ef619020646b7cd4310332aaf1e4be26b69
myusername <pabblo92@gmail.com>
2023-05-03 21:35:12 +0200

    RC-5 Developed divide request

--- src/main/java/com/uclm/restcalculator/controllers/CalculatorController.java
+++ src/main/java/com/uclm/restcalculator/controllers/CalculatorController.java
@@ -1,5 +1,6 @@
 package com.uclm.restcalculator.controllers;
 
+import com.uclm.restcalculator.exceptions.IllegalParameterException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,13 @@ public class CalculatorController {
     @GetMapping("/multiply")
     public double multiply(@RequestParam double a, @RequestParam double b) {
         return a * b;
+    }
+
+    @GetMapping("/divide")
+    public double divide(@RequestParam double a, @RequestParam double b) throws IllegalParameterException {
+        if (b == 0)
+            throw new IllegalParameterException("Divisor cannot be zero");
+
+        return a / b;
     }
 }
